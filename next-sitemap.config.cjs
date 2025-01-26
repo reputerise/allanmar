2e7f4b40f91780739007fb2a4b20ea0d2a95b97a
dpipke-setandforget <daniela@setandforget.io>
2025-01-25 22:47:00 -0300

    fix sitemap

--- next-sitemap.config.cjs
+++ next-sitemap.config.cjs
@@ -19,7 +19,7 @@ async function fetchDynamicRoutes() {
   const res = await fetch('https://xy1nu7wy.api.sanity.io/v1/data/query/production?query=*[_type == "post"]{slug}'); // URL donde obtienes los slugs
   const posts = await res.json();
   return posts.result.map(post => ({
-      loc: `/${post.slug.current}`,
+      loc: `/blog/${post.slug.current}`,
       lastmod: new Date().toISOString(),
   }));
 }
