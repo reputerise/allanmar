431fe7f262cf94ba4ad96f4ddfbc63da885aef8e
dpipke-setandforget <daniela@setandforget.io>
2025-02-02 16:22:47 -0300

    added head canonical

--- src/app/[slug]/layout.jsx
+++ src/app/[slug]/layout.jsx
@@ -2,20 +2,21 @@ import { fetchMetadata } from '../../components/blog/fetchMetadata';
 
 export async function generateMetadata({ params }) {
     const postMetadata = await fetchMetadata(params.slug);
+    const canonicalUrl = `https://blog.futerman.com.ar/${params.slug}`;
 
     return {
         title: postMetadata.title,
         description: postMetadata.description,
+        alternates: {
+            canonical: canonicalUrl,
+        },
     };
 }
 
 export default async function BlogLayout({ children }) {
     return (
-        <>
-
-            <div className="blog-layout">
-                {children}
-            </div>
-        </>
+        <div className="blog-layout">
+            {children}
+        </div>
     );
 }
