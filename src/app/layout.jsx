f1000f54b5b902409187662831a42676b01fc98e
dpipke-setandforget <daniela@setandforget.io>
2025-02-14 19:52:56 -0300

    added gtm

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -47,7 +47,24 @@ export default function RootLayout({ children }) {
           gtag('config', 'G-2XSBR9TNS5');
         `}
       </Script>
+      <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-M3KP2PXJ');
+          `}
+        </Script>
       <body className={`${montserrat.className} montserrat relative`}>
+      <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-M3KP2PXJ"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
         <Navbar />
         {children}
         <Footer />
